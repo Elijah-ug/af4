3dd7eac675b1c376da5ada93ec4f83dd39bf687a
elijah-ug <elijahtalentmugisha14@gmail.com>
2026-02-22 01:30:46 +0300

    modified docker files

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 echo "Running migrations"
-until nc -z db 5432; do
-  sleep 2
-done
+# dev
+# until nc -z db 5432; do
+# prod
+# until nc -z dpg-d6d24np4tr6s73chtleg-a 5432; do
+  # sleep 2
+# done
 npx prisma migrate deploy
 echo "starting the service"
 npm run start
